8cc96711e87f18c24e42361f7dfec0e8ce2c2c6a
Johann-butler <bjornkorczak@gmail.com>
2026-09-26 15:01:54 +0200

    Delete Test/johann-v25-11-script-3.js

--- Test/johann-v25-11-script-3.js
+++
@@ -1,3 +0,0 @@
-/* Isolierter Test: Analytics und Admin-Dashboard bewusst deaktiviert. */
-window.johannTrack=()=>{};
-const privacyConsent=document.getElementById("privacyConsent");if(privacyConsent)privacyConsent.hidden=true;
